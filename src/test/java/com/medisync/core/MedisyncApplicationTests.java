43b91bd7e74548f227dfc0880038b69d5c6844bf
mbd888 <soccerkingmd03@gmail.com>
2025-10-05 13:58:39 -0700

    Initial commit, project setup

---
+++ src/test/java/com/medisync/core/MedisyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisync.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
